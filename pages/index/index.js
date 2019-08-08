031e53d6cd96b18a1ba831f7f563212350ec1405
长安的小公举 <overcome_1995@yeah.net>
2019-08-08 18:14:08 +0800

    first commit

--- pages/index/index.js
+++ pages/index/index.js
@@ -5,5 +5,77 @@ data:{
     "../images/banner_2.jpg",
     "../images/banner_3.jpg",
     "../images/banner_4.jpg"
-  ]}
+  ]},
+  yuyuedetail1 : function(){
+    wx.navigateTo({
+      url: '../yuyuedetail1/yuyuedetail1',
+      success: function (res) { },
+      fail: function (res) { },
+      complete: function (res) { },
+    })
+  },
+  yuyuedetail2: function () {
+    wx.navigateTo({
+      url: '../yuyuedetail2/yuyuedetail2',
+      success: function (res) { },
+      fail: function (res) { },
+      complete: function (res) { },
+    })
+  },
+  yuyuedetail3: function () {
+    wx.navigateTo({
+      url: '../yuyuedetail3/yuyuedetail3',
+      success: function (res) { },
+      fail: function (res) { },
+      complete: function (res) { },
+    })
+  },
+  yuyuedetail4: function () {
+    wx.navigateTo({
+      url: '../yuyuedetail4/yuyuedetail4',
+      success: function (res) { },
+      fail: function (res) { },
+      complete: function (res) { },
+    })
+  },
+  yuyuedetail5: function () {
+    wx.navigateTo({
+      url: '../yuyuedetail5/yuyuedetail5',
+      success: function (res) { },
+      fail: function (res) { },
+      complete: function (res) { },
+    })
+  },
+  yuyuedetail6: function () {
+    wx.navigateTo({
+      url: '../yuyuedetail6/yuyuedetail6',
+      success: function (res) { },
+      fail: function (res) { },
+      complete: function (res) { },
+    })
+  },
+  yuyuedetail7: function () {
+    wx.navigateTo({
+      url: '../yuyuedetail7/yuyuedetail7',
+      success: function (res) { },
+      fail: function (res) { },
+      complete: function (res) { },
+    })
+  },
+  yuyuedetail8: function () {
+    wx.navigateTo({
+      url: '../yuyuedetail8/yuyuedetail8',
+      success: function (res) { },
+      fail: function (res) { },
+      complete: function (res) { },
+    })
+  },
+  yuyuedetail9: function () {
+    wx.navigateTo({
+      url: '../yuyuedetail9/yuyuedetail9',
+      success: function (res) { },
+      fail: function (res) { },
+      complete: function (res) { },
+    })
+  },
 })
--- pages/my/my.js
+++ pages/my/my.js
@@ -1,15 +1,55 @@
-//logs.js
-const util = require('../../utils/util.js')
-
+//js
 Page({
-  data: {
-    logs: []
-  },
-  onLoad: function () {
-    this.setData({
-      logs: (wx.getStorageSync('logs') || []).map(log => {
-        return util.formatTime(new Date(log))
-      })
+  yuyuerecord: function () {
+    wx.navigateTo({
+      url: '../yuyuerecord/yuyuerecord',
+      success: function (res) { },
+      fail: function (res) { },
+      complete: function (res) { },
     })
-  }
+  },
+  myposition: function (){
+    wx:wx.navigateTo({
+      url: '../myposition/myposition',
+      success: function(res) {},
+      fail: function(res) {},
+      complete: function(res) {},
+    })
+  },
+  roominformation: function (){
+    wx.navigateTo({
+      url: '../roominformation/roominformation',
+      success: function (res) { },
+      fail: function (res) { },
+      complete: function (res) { },
+    })
+  },
+
+  shiyonginstructions: function () {
+    wx.navigateTo({
+      url: '../shiyonginstructions/shiyonginstructions',
+      success: function (res) { },
+      fail: function (res) { },
+      complete: function (res) { },
+    })
+  },
+
+  suggestion: function () {
+    wx.navigateTo({
+      url: '../suggestion/suggestion',
+      success: function (res) { },
+      fail: function (res) { },
+      complete: function (res) { },
+    })
+  },
+  
+  aboutus: function () {
+    wx.navigateTo({
+      url: '../aboutus/aboutus',
+      success: function (res) { },
+      fail: function (res) { },
+      complete: function (res) { },
+    })
+  },
+
 })
